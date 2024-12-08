6d394f423630180329fc1f33c72e544d2f81312f
Elizabeth Mendez <40727857+zabalae@users.noreply.github.com>
2024-12-08 18:02:10 -0400

    task 2

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -1,13 +1,15 @@
 import $ from "jquery";
-import _ from "lodash";
+import _ from 'lodash';
+import '../css/main.css';
 
+$('<div id="logo"></div>').prependTo("body");
 $('<p>Holberton Dashboard</p>').appendTo("body");
 $('<p>Dashboard data for the students</p>').appendTo("body");
 $('<button>Click here to get started</button>').appendTo("body");
 $("<p id='count'></p>").appendTo("body");
 $('<p>Copyright - Holberton School</p>').appendTo("body");
 
-$('button').on('click', _.debounce(updateCounter, 500)); // ["500"]
+$('button').on('click', _.debounce(updateCounter));
 
 let count = 0;
 function updateCounter() {
