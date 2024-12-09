a63686b7b23c0b9a6d40d11acea89046f3d98d13
Elizabeth Mendez <40727857+zabalae@users.noreply.github.com>
2024-12-08 21:39:19 -0400

    task 3

---
+++ Webpack/task_3/modules/header/header.js
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+
+import './header.css';
+
+$('body').append('<header></header>');
+$('header').append('<div id="logo"></div>',
+                   '<h1>Holberton Dashboard</h1>');
+
+console.log('Init header');
