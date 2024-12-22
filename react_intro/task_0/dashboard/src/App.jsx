f6c92cbeddb0abfe70eaf6aedbeb42cc5f7c8de3
Elizabeth Mendez <40727857+zabalae@users.noreply.github.com>
2024-12-22 19:19:21 -0400

    task 0

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css'; // Make sure the path is correct
+import logo from './assets/holberton-logo.jpg'; // Adjust the path as needed
+import { getCurrentYear, getFooterCopy } from './utils';
+import Notifications from './Notifications';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <Notifications />
+      <header className="App-header">
+        <img src={logo} alt="Holberton logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <footer className="App-footer">
+        <p>{getFooterCopy(true)} - Copyright {getCurrentYear()} - Holberton School</p>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
